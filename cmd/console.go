5aee95019f5417638d2f0295e013d2353cbc83f3
Ricky004 <tanaydam000@gmail.com>
2024-06-26 09:43:24 +0530

    modified the output with color and add console

---
+++ cmd/console.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/fatih/color"
+)
+
+type Verbosity int
+
+const (
+	Quiet Verbosity = iota
+	Normal
+)
+
+type Console struct {
+	verbosity Verbosity
+}
+
+func NewConsoleWithVerbosity(verbosity Verbosity) *Console {
+	return &Console{
+		verbosity: verbosity,
+	}
+}
+
+func NewConsole() *Console {
+	return &Console{
+		verbosity: Normal,
+	}
+}
+
+func (c *Console) PrintMessage(message string) {
+	if c.verbosity == Quiet {
+		return 
+	}
+	fmt.Print(message)
+}
+
+func (c *Console) PrintError(errorMessage string) {
+	fmt.Print(errorMessage)
+}
+
+func (c *Console) PrintReplacement(prefix string, replacement *Replacement) {
+	redUnderline := color.New(color.FgRed, color.Underline).SprintFunc()
+	greenUnderline := color.New(color.FgGreen, color.Underline).SprintFunc()
+
+	fragments := replacement.GetFragments().data
+
+	inputFragments := make([]Fragment, len(fragments))
+	outputFragments := make([]Fragment, len(fragments))
+
+	for i, frag := range fragments {
+		inputFragments[i] = frag[0]
+		outputFragments[i] = frag[1]
+	}
+
+	redPrefix := fmt.Sprintf("%s%s", prefix, color.New(color.FgRed).Sprint("- "))
+    c.printFragments(redPrefix, redUnderline, replacement.ReplacementInput(), inputFragments)
+
+	greenPrefix := fmt.Sprintf("%s%s", prefix, color.New(color.FgGreen).Sprint("+ "))
+    c.printFragments(greenPrefix, greenUnderline, replacement.ReplacementOutput(), outputFragments)
+}
+
+func (c *Console) printFragments(prefix string, colorFunc func(a ...interface{}) string, line string, fragments []Fragment) {
+	c.PrintMessage(prefix)
+	currentIndex := 0
+	for i, fragment := range fragments {
+		index := fragment.Index
+		text := fragment.Text
+
+		if index < 0 || index > len(line) {
+			continue
+		}
+
+		if i == 0 {
+			c.PrintMessage(strings.TrimLeft(line[currentIndex:index], " "))
+		} else {
+			c.PrintMessage(line[currentIndex:index])
+		}
+
+		endIndex := index + len(text)
+		if endIndex > len(line) {
+            endIndex = len(line)
+		}
+
+		c.PrintMessage(colorFunc(text))
+		currentIndex = endIndex
+	}
+    
+	if currentIndex < len(line) {
+		c.PrintMessage(line[currentIndex:])
+	}
+    
+	if !strings.HasSuffix(line, "\n") {
+		c.PrintMessage("\n")
+	}
+}
--- cmd/main.go
+++ cmd/main.go
@@ -3,37 +3,58 @@ package main
 import (
     "fmt"
 	"regexp"
-
+    
+	"github.com/fatih/color"
 )
 
 func main() {
-	substringQuery := NewSubString("hello", "hii")
-	regexQuery := NewRegex(regexp.MustCompile(`(tridip|hello)`), "fuck")
-	subvertQuery := NewSubvert("foo_bar", "spam_eggs")
+	substringQuery := NewSubString("hello", "hi")
+	regexQuery := NewRegex(regexp.MustCompile(`old`), "new")
+	subvertQuery := NewSubvert("case", "casecase")
 
-	input := "hello my name is foo_bar and my freind name is FooBar and his friend name is foo-bar"
+	input := "This is a dummy text [hello] [old] []"
 
 	fmt.Println("Substring Query:")
 	replacement := Replace(input, substringQuery)
 	if replacement != nil {
-		fmt.Println("Output:", replacement.ReplacementOutput())
+		c := color.New(color.FgHiGreen).Add(color.Bold)
+		c.Printf("Output: %s\n", replacement.ReplacementOutput())
 	} else {
-		fmt.Println("No replacement made.")
+		c := color.New(color.FgRed).Add(color.Bold)
+		c.Println("No replacement made.")
 	}
 
 	fmt.Println("\nRegex Query:")
 	replacement = Replace(input, regexQuery)
 	if replacement != nil {
-		fmt.Println("Output:", replacement.ReplacementOutput())
+		c := color.New(color.FgHiMagenta).Add(color.Bold)
+		c.Printf("Output: %s\n", replacement.ReplacementOutput())
 	} else {
-		fmt.Println("No replacement made.")
+		c := color.New(color.FgRed).Add(color.Bold)
+		c.Println("No replacement made.")
 	}
 
 	fmt.Println("\nSubvert Query:")
 	replacement = Replace(input, subvertQuery)
 	if replacement != nil {
-		fmt.Println("Output:", replacement.ReplacementOutput())
+		c := color.New(color.FgHiYellow).Add(color.Bold)
+		c.Printf("Output: %s\n", replacement.ReplacementOutput())
 	} else {
-		fmt.Println("No replacement made.")
+		c := color.New(color.FgRed).Add(color.Bold)
+		c.Println("No replacement made.")
 	}
+
+	// console := NewConsoleWithVerbosity(Normal)
+
+	// fragments := &Fragments{
+	// 	data: [][2]Fragment{
+	// 		{Fragment{20, "colored"}, Fragment{20, "colourful"}},
+	// 		{Fragment{28, "text"}, Fragment{30, "words"}},
+	// 	},
+	// }
+
+	// replacement1 := NewReplacement("This is a line with colored text fragments.", fragments, "This is a line with colourful words fragments.")
+
+	// console.PrintReplacement("", replacement1)
+
 }
